78cb06c96bba7ee46c4481ab393010bbeb6b8880
Nettakrim <you@example.com>
2023-04-04 18:14:02 +0100

    added random sounds and fixed bug where i accidentally broke the creeper shader

--- src/main/java/com/nettakrim/souper_secret_settings/mixin/StewItemMixin.java
+++ src/main/java/com/nettakrim/souper_secret_settings/mixin/StewItemMixin.java
@@ -11,12 +11,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.nettakrim.souper_secret_settings.RandomSound;
 import com.nettakrim.souper_secret_settings.SouperSecretSettingsClient;
 
 @Mixin(StewItem.class)
 public class StewItemMixin {
 	@Inject(at = @At("HEAD"), method = "finishUsing")
 	private void finishUsing(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
-		if (world.isClient && stack.isOf(Items.BEETROOT_SOUP)) SouperSecretSettingsClient.setShader("random");
+		if (world.isClient && stack.isOf(Items.BEETROOT_SOUP)) {
+			SouperSecretSettingsClient.setShader("random");
+			RandomSound.Play();
+		}
 	}
 }
