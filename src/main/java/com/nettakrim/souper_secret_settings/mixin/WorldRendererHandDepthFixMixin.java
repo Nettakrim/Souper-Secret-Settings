4994560aa28cf9865dd2d8ea24498fb30299d553
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-03-09 02:39:49 +1300

    Added Fabulous graphics support for Hand Renderer - however, transparent blocks won't get rendered behind coloured glass.

--- src/main/java/com/nettakrim/souper_secret_settings/mixin/WorldRendererHandDepthFixMixin.java
+++ src/main/java/com/nettakrim/souper_secret_settings/mixin/WorldRendererHandDepthFixMixin.java
@@ -14,7 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererHandDepthFixMixin {
-	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;renderParticles(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/render/LightmapTextureManager;Lnet/minecraft/client/render/Camera;F)V", shift = At.Shift.AFTER), method = "render")
+	@Inject(at = {
+			@At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderPhase$Target;endDrawing()V", ordinal = 0, shift = At.Shift.AFTER),
+			@At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;renderParticles(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/render/LightmapTextureManager;Lnet/minecraft/client/render/Camera;F)V", ordinal = 1, shift = At.Shift.AFTER)
+	}, method = "render")
 	public void fixHandRenderer(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f projectionMatrix, CallbackInfo ci) {
 		SouperSecretSettingsClient.getGameRendererAccessor().invokeRenderHand(SouperSecretSettingsClient.renderHandMatrixStack, camera, tickDelta);
 	}
