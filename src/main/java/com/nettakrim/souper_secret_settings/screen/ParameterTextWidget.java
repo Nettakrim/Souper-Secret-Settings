0b658b15668a3f34862747bfb466b6b2b985252c
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-10-10 13:03:10 +0100

    parameter text widgets can now be dragged to change value

--- src/main/java/com/nettakrim/souper_secret_settings/screen/ParameterTextWidget.java
+++ src/main/java/com/nettakrim/souper_secret_settings/screen/ParameterTextWidget.java
@@ -8,4 +8,12 @@ public class ParameterTextWidget extends TextFieldWidget {
     public ParameterTextWidget(TextRenderer textRenderer, int x, int width, int height, Text message) {
         super(textRenderer, x, 0, width, height, message);
     }
+
+    @Override
+    protected void onDrag(double mouseX, double mouseY, double deltaX, double deltaY) {
+        try {
+            float f = Float.parseFloat(getText());
+            setText(Float.toString(f + (float)(deltaX/50.0 * Math.max(Math.abs(f), 0.5f))));
+        } catch (Exception ignored) {}
+    }
 }
