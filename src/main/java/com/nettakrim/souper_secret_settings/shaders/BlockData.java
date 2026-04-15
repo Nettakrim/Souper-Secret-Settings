2eb101fc2308851fe2932091faed4f8ac15c3d73
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2026-04-15 21:56:52 +0100

    fixed ascii, mouse, bloom_color, xor, s2xe

--- src/main/java/com/nettakrim/souper_secret_settings/shaders/BlockData.java
+++ src/main/java/com/nettakrim/souper_secret_settings/shaders/BlockData.java
@@ -8,6 +8,7 @@ import com.mclegoman.luminance.client.shaders.overrides.*;
 import com.mclegoman.luminance.client.shaders.uniforms.Uniform;
 import com.mclegoman.luminance.client.shaders.uniforms.UniformVector;
 import com.mclegoman.luminance.client.shaders.uniforms.config.MapConfig;
+import com.nettakrim.souper_secret_settings.SouperSecretSettingsClient;
 import net.minecraft.resources.Identifier;
 
 import java.util.*;
@@ -59,8 +60,40 @@ public class BlockData {
             MapConfig configOverride = new MapConfig(Map.of());
             PerValueOverride defaultOverride = null;
 
-            if (uniformInstance.override instanceof PerValueOverride luminanceUniformOverride) {
-                defaultOverride = luminanceUniformOverride;
+            // copy over config, changing any un-indexed values to be indexed
+            if (uniformInstance.config != null) {
+                Set<String> names = uniformInstance.config.getNames();
+                for (String name : names) {
+                    List<Object> objects = uniformInstance.config.getObjects(name);
+                    if (objects == null) {
+                        continue;
+                    }
+                    int separator = name.indexOf('_');
+
+                    boolean needsIndexing = true;
+                    if (separator > 0) {
+                        try {
+                            int index = Integer.parseInt(name.substring(0, separator));
+                            if (index >= 0 && index <= uniformInstance.length()) {
+                                needsIndexing = false;
+                            }
+                        } catch (NumberFormatException ignored) {
+                        }
+                    }
+
+                    if (needsIndexing) {
+                        for (int i = 0; i < uniformInstance.length(); i++) {
+                            configOverride.config().putIfAbsent(i + "_" + name, objects);
+                        }
+                    } else {
+                        configOverride.config().put(name, objects);
+                    }
+                }
+            }
+
+            // get override as a PerValueOverride
+            if (uniformInstance.override instanceof PerValueOverride perValueOverride) {
+                defaultOverride = perValueOverride;
             }
             if (defaultOverride == null) {
                 defaultOverride = new PerValueOverride(List.of());
@@ -75,10 +108,13 @@ public class BlockData {
                     continue;
                 }
 
+                // add template overrides if they are needed
                 OverrideSource overrideSource = defaultOverride.overrideSources.get(i);
                 if (overrideSource instanceof UniformSource uniformSource) {
                     defaultOverride.overrideSources.set(i, new ParameterOverrideSource(uniformSource));
+                    // get uniform range, since the template will have it as 0 and 1
                     configOverride.config().putIfAbsent(i + "_range", getRange(uniformSource));
+                    // copy template values
                     PerValueConfig perValueConfig = new PerValueConfig(overrideSource.getTemplateConfig(), i);
                     perValueConfig.setIndex(-1);
                     configOverride.mergeWithConfig(perValueConfig);
